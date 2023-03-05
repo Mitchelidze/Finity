4decc87fcbe2a778b723ca9822bd49e439cf5b29
Miranda Tchelidze <mitchelidze@gmail.com>
2023-03-05 19:04:39 +0400

    Added tableViewCell on homeVC, Set UI

---
+++ Finity/Presentation/Home/BudgetingCell.swift
@@ -0,0 +1,25 @@
+//
+//  BudgetingCell.swift
+//  Finity
+//
+//  Created by michelidze'smacbook on 05.03.23.
+//
+
+import UIKit
+
+class BudgetingCell: UITableViewCell {
+    
+    var identifier = "BudgetingCell"
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+    
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+}
--- Finity/Presentation/Home/HomeViewController.swift
+++ Finity/Presentation/Home/HomeViewController.swift
@@ -7,23 +7,25 @@
 
 import UIKit
 
-class HomeViewController: UIViewController {
+class HomeViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UINib(nibName: "BudgetingCell", bundle: nil), forCellReuseIdentifier: "BudgetingCell")
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        1
     }
-    */
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "BudgetingCell", for: indexPath) as! BudgetingCell
+        return cell
+    }
 
 }
