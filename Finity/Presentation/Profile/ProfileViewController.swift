0e0ad94c647ff96661ad4d815a8436240c22490f
Miranda Tchelidze <mitchelidze@gmail.com>
2023-03-20 19:40:20 +0400

    Added GoogleServiceInfo plist file, installed Firebase iOS SDK, Added Sign up on profile page, added firebaseAuth

--- Finity/Presentation/Profile/ProfileViewController.swift
+++ Finity/Presentation/Profile/ProfileViewController.swift
@@ -6,24 +6,72 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class ProfileViewController: UIViewController {
-
+    @IBOutlet weak var titleLabelView: UILabel!
+    @IBOutlet weak var subTitleLabelView: UILabel!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func onSignUp(_ sender: Any) {
+        print("Sign up button tapped")
+        guard let email = emailTextField.text, !email.isEmpty,
+              let password = passwordTextField.text, !password.isEmpty else {
+            print("fields are empty")
+            return
+        }
+        
+        FirebaseAuth.Auth.auth().signIn(withEmail: email, password: password, completion: { [weak self] result, error in
+            guard let strongSelf = self else {
+                return
+            }
+            
+            guard error == nil else {
+                strongSelf.showCreateAccount(email: email, password: password)
+                return
+            }
+            print("signed in")
+            strongSelf.passwordTextField.isHidden = true
+            strongSelf.emailTextField.isHidden = true
+            strongSelf.titleLabelView.isHidden = true
+            strongSelf.subTitleLabelView.isHidden = true
+        })
     }
-    */
-
+    
+    func showCreateAccount(email: String, password: String) {
+        let alert = UIAlertController(title: "Create Account", message: "Would you like to create account", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Contionue", style: .default, handler: {_ in
+            FirebaseAuth.Auth.auth().createUser(withEmail: email, password: password, completion: { [weak self] result, error in
+                
+                guard let strongSelf = self else {
+                    return
+                }
+                
+                guard error == nil else {
+                    print("signing up filed: \(error!.localizedDescription)")
+                    return
+                }
+                print("signed in")
+                strongSelf.passwordTextField.isHidden = true
+                strongSelf.emailTextField.isHidden = true
+                strongSelf.titleLabelView.isHidden = true
+                strongSelf.subTitleLabelView.isHidden = true
+            })
+            
+        }))
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: {_ in
+            
+        }))
+        
+        present(alert, animated: true)
+    }
+                        
 }
