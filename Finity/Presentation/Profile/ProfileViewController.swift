70a29d8e2aa15691760773f139b2af8c46a36093
Miranda Tchelidze <mitchelidze@gmail.com>
2023-03-21 16:23:54 +0400

    Updated login and sign up pages

--- Finity/Presentation/Profile/ProfileViewController.swift
+++ Finity/Presentation/Profile/ProfileViewController.swift
@@ -14,94 +14,65 @@ class ProfileViewController: UIViewController {
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var signUpButton: UIButton!
+    @IBOutlet weak var alreadyHaveAnAccountLabelView: UILabel!
+    @IBOutlet weak var logInButton: UIButton!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-//        if FirebaseAuth.Auth.auth().currentUser != nil {
-//            passwordTextField.isHidden = true
-//            emailTextField.isHidden = true
-//            titleLabelView.isHidden = true
-//            subTitleLabelView.isHidden = true
-//            signUpButton.isHidden = true
-//
-//            let signOutButton: UIButton = {
-//                let button = UIButton()
-//                button.backgroundColor = .systemBlue
-//                button.setTitleColor(.white, for: .normal)
-//                button.setTitle("Log out", for: .normal)
-//                return button
-//            }()
-//
-//            view.addSubview(signOutButton)
-//            signOutButton.frame = CGRect(x: 20, y: 150, width: view.frame.size.width - 40, height: 48)
-//            signOutButton.addTarget(self, action: #selector(logOutTapped), for: .touchUpInside)
-//        }
 
     }
     
     
-
+    @IBAction func onLogIn(_ sender: Any) {
+        print("tapped log in button")
+        titleLabelView.text = "Log in to Finity Account"
+        subTitleLabelView.isHidden = true
+        signUpButton.setTitle("Log in", for: .normal)
+        alreadyHaveAnAccountLabelView.text = "Don't have an account?"
+        logInButton.setTitle("Sign up", for: .normal)
+        
+    }
+    
     @IBAction func onSignUp(_ sender: Any) {
         print("Sign up button tapped")
         guard let email = emailTextField.text, !email.isEmpty,
               let password = passwordTextField.text, !password.isEmpty else {
-            print("fields are empty")
+            
+            showAlert(message: "Please enter the email and password")
             return
         }
         
-        FirebaseAuth.Auth.auth().signIn(withEmail: email, password: password, completion: { [weak self] result, error in
+        FirebaseAuth.Auth.auth().createUser(withEmail: email, password: password, completion: { [weak self] result, error in
+
             guard let strongSelf = self else {
                 return
             }
-            
-//            guard error == nil else {
-//                strongSelf.showCreateAccount(email: email, password: password)
-//                return
-//            }
+
+            guard error == nil else {
+                print("signing up filed: \(error!.localizedDescription)")
+                return
+            }
             print("signed in")
             strongSelf.passwordTextField.isHidden = true
             strongSelf.emailTextField.isHidden = true
             strongSelf.titleLabelView.isHidden = true
             strongSelf.subTitleLabelView.isHidden = true
             strongSelf.signUpButton.isHidden = true
-            
+            strongSelf.alreadyHaveAnAccountLabelView.isHidden = true
+            strongSelf.logInButton.isHidden = true
+
+
             strongSelf.emailTextField.resignFirstResponder()
             strongSelf.passwordTextField.resignFirstResponder()
         })
     }
     
-//    func showCreateAccount(email: String, password: String) {
-//        let alert = UIAlertController(title: "Create Account", message: "Would you like to create account", preferredStyle: .alert)
-//        alert.addAction(UIAlertAction(title: "Contionue", style: .default, handler: {_ in
-//            FirebaseAuth.Auth.auth().createUser(withEmail: email, password: password, completion: { [weak self] result, error in
-//
-//                guard let strongSelf = self else {
-//                    return
-//                }
-//
-//                guard error == nil else {
-//                    print("signing up filed: \(error!.localizedDescription)")
-//                    return
-//                }
-//                print("signed in")
-//                strongSelf.passwordTextField.isHidden = true
-//                strongSelf.emailTextField.isHidden = true
-//                strongSelf.titleLabelView.isHidden = true
-//                strongSelf.subTitleLabelView.isHidden = true
-//                strongSelf.signUpButton.isHidden = true
-//
-//
-//                strongSelf.emailTextField.resignFirstResponder()
-//                strongSelf.passwordTextField.resignFirstResponder()
-//            })
-//
-//        }))
-//        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: {_ in
-//
-//        }))
-//
-//        present(alert, animated: true)
-//    }
+    func showAlert(message: String) {
+        let alert = UIAlertController(title: "Required field", message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "Got it", style: .default, handler: nil)
+        alert.addAction(okAction)
+        present(alert, animated: true, completion: nil)
+    }
                         
 }
